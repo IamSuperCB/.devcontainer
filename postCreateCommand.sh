b441d8f661e4ed549e93a159d96c1e19f735a0cd
Carlos Betancourt <carlos@betancourtca.net>
2023-12-14 12:50:31 -0500

    Reactivated dotnet feature

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -1,3 +1,4 @@
+sudo chown $LOGNAME:root /workspace
 cd /workspace
 sudo apt update -y
 sudo apt install -y build-essential net-tools dnsutils traceroute jupyter keychain mtr software-properties-common apt-transport-https wget libsecret-1-0
@@ -11,10 +12,3 @@ ijsinstall
 npm install -g node-gyp 
 cd /home/carlo/.nvm/versions/node/v18.18.2/lib/node_modules/ijavascript/node_modules/zeromq
 node-gyp rebuild
-
-sudo chown $LOGNAME:root /workspace
-
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-sudo chmod +x ./dotnet-install.sh
-./dotnet-install.sh --channel STS
-rm dotnet-install.sh
