b1ec1ecb0f2fbbeb750f26d5215181fca60b8ec7
Carlos Betancourt <carlos@betancourtca.net>
2023-12-15 10:34:18 -0500

    Refactor from @iamsupercb/fos/.devcontainer Fixes #1 misc updates

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -2,10 +2,6 @@ sudo chown $LOGNAME:root /workspace
 cd /workspace
 sudo apt update -y
 sudo apt install -y build-essential net-tools dnsutils traceroute jupyter keychain mtr software-properties-common apt-transport-https wget libsecret-1-0
-# curl -fSsL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-edge.gpg > /dev/null
-# echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
-# sudo apt update -y
-# sudo apt install -y microsoft-edge-stable
 
 npm install -g yarn ijavascript
 ijsinstall
